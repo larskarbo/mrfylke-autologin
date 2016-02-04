ac763ff7c01eac1b7c94c245dfa9cc7c31af268b
Lars <larskarbo@hotmail.com>
2016-02-04 22:13:53 +0100

    update

--- popup.js
+++ popup.js
@@ -1,28 +1,41 @@
 $(document).ready(function(){
 
+  var DEFAULT_URL = 'http://ungweb.no'
 
-  //localStorage["username"] = 'yolo';
   var username;
-  chrome.storage.local.get(['disable','username','password'], function(result){
+
+  if($('#redirect').val().length == 0){
+    $('#redirect').val(DEFAULT_URL)
+  }
+
+  chrome.storage.local.get(['disable','username','password','redirect'], function(result){
 
   	$('#username').val(result.username);
-  	$('#password').val(result.password);
+    $('#password').val(result.password);
+    $('#redirect').val(result.redirect);
 
-  	if(!result.disable){
-  		$('#disable').prop('checked',false);
-  	}
+    if(!result.disable){
+      $('#disable').prop('checked',false);
+    }
   })
 
   $('#save').click(function(){
-  	chrome.storage.local.set({
-  		'username': $('#username').val(),
-  		'password': $('#password').val()
-  	}, function() {
+    if($('#redirect').val().length == 0){
+      $('#redirect').val(DEFAULT_URL)
+    }
+    chrome.storage.local.set({
+      'username': $('#username').val(),
+      'password': $('#password').val(),
+      'redirect': $('#redirect').val()
+    }, function() {
   		// Notify that we saved.
   		console.log('creds saved');
-  		$('#message').html('creds saved');
+  		$('#message').html('save successfull');
+      setTimeout(function(){
+        $('#message').html('');
+      }, 2000)
 
-  	});
+    });
   });
 
   $('#disable').on('change',function(){
--- script.js
+++ script.js
@@ -12,23 +12,23 @@ function enter(){
 	$('#saveAndSubmit').after('<div id="message"></div>');
 	
 	$('#saveAndSubmit').on('click',saveAndSubmit);
-    
-    var creds;
 
-    getLocalCreds(function(result){
-    	console.log(result);
-    	if(result){
+	var creds;
+
+	getLocalCreds(function(result){
+		console.log(result);
+		if(result){
 	        //cookies are set, log me in;
 	        console.log('creds found');
 	        logIn(result.username,result.password);
 
-    	}else{
+	    }else{
         	//we need to set a password
-       		console.log('no creds found');
-    	}
-    
-   
-	});
+        	console.log('no creds found');
+        }
+
+
+    });
 }
 
 setMessage = function(){
@@ -59,84 +59,108 @@ setMessage = function(){
 
 }();
 
-function hasError(text){
-    var html = document.getElementsByTagName('html')[0].innerHTML;
-    
-    if(html.search('Login Error') <= 0){
-        return true;
+function getResponse(html){
+    console.log($(html).find('input[name=err_flag]').val());
+	console.log('html',html);
+	if(html.search('getErrorMsgIfAny') != -1){
+		return 'error';
+	}else if(html.search('Logged in') != -1){
+		return 'success';
+	}else
+    	return 'already'; //already logged in
     }
-    
-    return false;
-}
-
-function getLocalCreds(callback) {
-	chrome.storage.local.get(['username','password'], function(result){
-		console.log(result);
-		if(Object.keys(result).length > 1 && result.username && result.password){
-			callback({
-				username:result.username,
-				password:result.password
-			});
-		}else{
-			callback(false);
-		}
-	})
-}
 
 
-function logIn(u,p){
-	console.log(u,p);
-	data = {
-		username:u,
-		password:p,
-		buttonClicked:4,
-		network_name:'Guest+Network'
-	}
-	
-	$.post( "login.html", data)
-	  .done(function(result) {
 
-	  	if(!hasError(result)){
+    function getLocalCreds(callback) {
+    	chrome.storage.local.get(['username','password'], function(result){
+    		console.log(result);
+    		if(Object.keys(result).length > 1 && result.username && result.password){
+    			callback({
+    				username:result.username,
+    				password:result.password
+    			});
+    		}else{
+    			callback(false);
+    		}
+    	});
+    }
 
-	  		console.log("success");
-			setMessage.success('dette gikk bra');
-			//window.location="http://ungweb.no/"
-	  	}else{
 
-	  		setMessage.error('ops, tullelure skjedde<br>sjekk brukernavn/passord');
-	  	}
-	  })
-	  .fail(function() {
-		setMessage.error('offadå noko dumt skjedde');
-	  })
-}
+    function logIn(u,p){
+    	console.log(u,p);
+    	data = {
+    		username:u,
+    		password:p,
+    		buttonClicked:4,
+    		network_name:'Guest+Network'
+    	}
 
-function saveCreds(u, p, callback){
-    chrome.storage.local.set({
-    	username:u,
-    	password:p
-    },callback)
-};
+    	$.post( "login.html", data)
+    	.done(function(result) {
+            console.log(result,'result');
+    		switch(getResponse(result)){
+    			case 'success':
+                case 'already':
+    			console.log('success');
+    			setMessage.success('Du er logga inn (jippi)');
+                redirect();
+    			break;
+    			case 'error':
+    			console.log('error');
+    			setMessage.success('ops, tullelure skjedde<br>sjekk brukernavn/passord');
+    			break;
+                // ikkje brukt no
+    			case 'already':
+    			console.log('alrady');
+    			setMessage.success('du er allerede innlogga :-(');
+                setTimeout(redirect, 1000);
+    				break;
 
-function saveAndSubmit(){
-    var u = document.querySelector('input[name=username]').value;
-    var p = document.querySelector('input[name=password]').value;
-	
-	saveCreds(u,p, function(){
-		logIn(u,p);
+    			}
 
-	});
-}
+    		})
+    	.fail(function() {
+    		setMessage.error('offadå noko dumt skjedde');
+    	})
+    }
 
-function submit(){
-    console.log('submitting');
-}
+    function saveCreds(u, p, callback){
+    	chrome.storage.local.set({
+    		username:u,
+    		password:p
+    	},callback)
+    };
 
-chrome.storage.local.get('disable', function(result){
-	console.log(result);
-	if(Object.keys(result).length > 0 && result.disable){
+    function redirect(){
+        chrome.storage.local.get(['redirect'], function(result){
+            if(result.redirect){
+                window.location = result.redirect;
+            }else{
+                window.location = 'http://ungweb.no';
+            }
+        });
+    }
 
-	}else{
-		enter();
-	}
-})
+    function saveAndSubmit(){
+    	var u = document.querySelector('input[name=username]').value;
+    	var p = document.querySelector('input[name=password]').value;
+
+    	saveCreds(u,p, function(){
+    		logIn(u,p);
+
+    	});
+    }
+
+    function submit(){
+    	console.log('submitting');
+    }
+
+    chrome.storage.local.get('disable', function(result){
+    	console.log(result);
+    	if(Object.keys(result).length > 0 && result.disable){
+
+    	}else{
+    		enter();
+    	}
+    })
